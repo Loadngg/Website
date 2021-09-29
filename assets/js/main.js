32b3f8d6f8693b3ec9df386f97397c1a216f64bc
Loadngg <averinj@mail.ru>
2021-09-29 19:02:08 +0300

    добавлен аккордеон и карта

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+// Accordion
+let accordion = document.getElementsByClassName('accordion__item');
+
+for (let i = 0; i < accordion.length; i++) {
+    accordion[i].firstElementChild.addEventListener('click', change)
+}
+
+function change(event) {
+    let targ = event.target;
+
+    if (targ.parentElement.classList.contains('accordion__item')) {
+        par = targ.parentElement;
+    } else {
+        par = targ.parentElement.parentElement;
+    }
+
+    if (par.classList.contains('active')) {
+        par.classList.remove('active')
+    } else {
+        for (let i = 0; i < accordion.length; i++) {
+            accordion[i].classList.remove('active')
+        }
+        par.classList.add('active')
+    }
+}
+
+
+// Nav scroll
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+}
